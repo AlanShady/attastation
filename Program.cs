dd55eaf04abd1cdf829458b543610dc72be16ce1
Shamil <shamilkarakotov@yandex.ru>
2024-01-21 12:48:10 +0300

    написали функцию заполенния массива

--- Program.cs
+++ Program.cs
@@ -14,3 +14,11 @@ void SecondArray()
         }
     }
 }
+void PrintArray(string[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+    Console.WriteLine();
+}
